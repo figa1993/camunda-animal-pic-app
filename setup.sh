fccbe3751dba5c9c062d1edcb559a64db4dff378
figa1993 <49098696+figa1993@users.noreply.github.com>
2024-08-20 20:53:46 +0000

    WIP need to do helm install on a better machine

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 # Install webapp dependencies
+pip install flask[async] pyzeebe
 
-pip install flask pyzeebe
+# Install Kubernetes & Helm
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+minikube start &
+
+# Install the Camunda chart
+helm repo add camunda https://helm.camunda.io
+helm install camunda-platform camunda/camunda-platform
--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify, request, render_template
+from pyzeebe import ZeebeClient, create_insecure_channel
+import asyncio
+
 app = Flask(__name__)
 
 # Mock database of images
@@ -8,14 +11,23 @@ images_db = {
     'option3': 'http://example.com/image3.jpg'
 }
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/get_image_url', methods=['GET'])
-def get_image_url():
+async def get_image_url():
     selection = request.args.get('selection')
+    
+    channel = create_insecure_channel(hostname='localhost',port='26500')  # Will use ZEEBE_ADDRESS environment variable or localhost:26500
+    client = ZeebeClient(channel)
+
+    process_instance_key, result = await client.run_process_with_result("Process_03kr6bi")
+
     return jsonify({'imageUrl': images_db.get(selection, '')})
+
+    # return result
 
 if __name__ == '__main__':
     app.run(debug=True)
